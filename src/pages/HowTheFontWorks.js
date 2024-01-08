820d4a5ced411ca46ba17a5504adfc16aff20a2d
Eric Hoover <ehoover3@yahoo.com>
2024-01-08 12:57:36 -0500

    version 1.1.1 replaced pdf bug with image

--- src/pages/HowTheFontWorks.js
+++ src/pages/HowTheFontWorks.js
@@ -5,7 +5,7 @@ const HowTheFontWorks = () => {
     <div>
       <h2>See The Font</h2>
       <div>Dyslexic friendly font puts a weight at the bottom of the characters to reduce letter rotation.</div>
-      <iframe title='OpenDyslexic Characters PDF' src='/pdfs/opendyslexic-characters.pdf' width='100%' height='500px' />
+      <img src='/images/abc.png' alt='The letters ABC in dyslexic friendly font' />
     </div>
   );
 };
