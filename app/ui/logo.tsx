bdaa0c7237068c0fc7cc5029c63502e6b30cbb1b
Eric Hoover <ehoover3@yahoo.com>
2024-05-25 22:21:11 -0400

    fix logo ui wrapping issue logo would not be inline when width is less than or equal to 767px; this is now resolved

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -5,7 +5,9 @@ export default function Logo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <p className="text-[44px]">Dyslexia Toolkit</p>
+      <p className="whitespace-nowrap text-[44px] md:whitespace-normal">
+        Dyslexia Toolkit
+      </p>
     </div>
   );
 }
